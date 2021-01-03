1097501b5557932523e7496154f0c1a1209d9245
Pietro Poluzzi <pit.pietro@protonmail.com>
2021-01-03 20:20:41 +0100

    iterate over the const char pointer

--- decode.c
+++ decode.c
@@ -7,5 +7,14 @@ const char *morse[MAX] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....
 const char *ascii[MAX] = {"A",  "B",    "C",    "D",   "E", "F",    "G",   "H",    "I",  "J",    "K",   "L",    "M",  "N",  "O",   "P",    "Q",    "R",   "S",   "T", "U",   "V",    "W",   "X",    "Y",    "Z",    "0",     "1",     "2",     "3",     "4",     "5",     "6",     "7",     "8",     "9",     ".",      ",",      "?",      "'",      "!",      "/",     "(",     ")",      "&",     ":",      ";",      "=",     "+",     "-",      "_",      "\"",     "$",       "@",      "SOS"};
 
 char *decode_morse(const char* morse_code) {
+    // iterate over the word given as parameter
+    // stop looping when we reach the null-character.
+    
+    size_t i = 0;
+    
+    while (morse_code[i] != '\0') {
+        printf("%c\n", morse_code[i]);
+        i++;
+    }
     return ascii[0];
 }
